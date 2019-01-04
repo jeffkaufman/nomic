2623344a4f2a121e79daa3e226b53acb38cf3e48
Jeff Kaufman <jeff@jefftk.com>
2019-01-04 02:16:52 +0000

    assume authors approve

--- validate.py
+++ validate.py
@@ -1,48 +1,58 @@
 import os
 import requests
 
+def request(url):
+  request_headers = {'User-Agent': 'jeffkaufman/nomic'}
+  response = requests.get(url, headers=request_headers)
+  response.raise_for_status()
+  return response.json()
+
 def base_pr_url():
   return 'https://api.github.com/repos/%s/pulls/%s' % (
     os.environ['TRAVIS_REPO_SLUG'],
     os.environ['TRAVIS_PULL_REQUEST'])
 
+def get_author():
+  response = request(base_pr_url())
+  return response['user']
+
 def get_reviews():
   url = '%s/reviews' % base_pr_url()
-  user_states = {}
+  reviews = {}
 
   while True:
-    request_headers = {'User-Agent': 'jeffkaufman/nomic'}
-    response = requests.get(url, headers=request_headers)
-    response.raise_for_status()
+    response = request(url)
 
-    for review in response.json():
+    for review in response:
       user = review['user']['login']
       state = review['state']
       if state == 'COMMENTED':
         continue
-      user_states[user] = state
+      reviews[user] = state
 
     if 'next' in response.links:
       url = response.links['next']['url']
     else:
-      return user_states
+      return reviews
 
 def get_users():
   users = set()
   with open('players.txt') as inf:
     for line in inf:
       users.add(line.strip())
-  return list(sorted(users))      
+  return list(sorted(users))
 
 def start():
+  author = get_author()
+  print ("Author: %s" % author)
+
   reviews = get_reviews()
-  if not reviews:
-    raise Exception('no reviews')
-  
+  reviews[author] = 'APPROVED'
+
   print ('Reviews:')
   for user, state in sorted(reviews.items()):
     print ('  %s: %s' % (user, state))
-    
+
   users = get_users()
   approval_count = 0
   for user in users:
