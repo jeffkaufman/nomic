d27580e78f5827f73795790fce994407ec7be78b
Jeff Kaufman <jeff@jefftk.com>
2019-01-28 20:11:56 -0500

    switch to python-style marking of private attributes

--- pull_request.py
+++ pull_request.py
@@ -4,14 +4,14 @@ import util
 
 class PullRequest:
   def __init__(self, repo, pr_number, target_commit, users):
-    self.repo_ = repo
-    self.pr_number_ = pr_number
-    self.target_commit_ = target_commit
-    self.users_ = users
+    self._repo = repo
+    self._pr_number = pr_number
+    self._target_commit = target_commit
+    self._users = users
 
-    self.pr_json_ = util.request(self.base_pr_url_()).json()
+    self._pr_json = util.request(self._base_pr_url()).json()
 
-    self.reviews = self.calculate_reviews_()
+    self.reviews = self._calculate_reviews()
     if self.author() in users:
       self.reviews[self.author()] = 'APPROVED'
 
@@ -30,10 +30,10 @@ class PullRequest:
                               and user not in self.rejections ]
 
   def created_at_ts(self):
-    return util.iso8601_to_ts(self.pr_json_['created_at'])
+    return util.iso8601_to_ts(self._pr_json['created_at'])
 
   def pushed_at_ts(self):
-    return util.iso8601_to_ts(self.pr_json_['head']['repo']['pushed_at'])
+    return util.iso8601_to_ts(self._pr_json['head']['repo']['pushed_at'])
 
   def last_changed_ts(self):
     return max(self.created_at_ts(),
@@ -48,8 +48,8 @@ class PullRequest:
   def days_since_changed(self):
     return util.days_since(self.last_changed_ts())
 
-  def calculate_reviews_(self):
-    base_url = '%s/reviews' % self.base_pr_url_()
+  def _calculate_reviews(self):
+    base_url = '%s/reviews' % self._base_pr_url()
     url = base_url
     reviews = {}
   
@@ -61,7 +61,7 @@ class PullRequest:
         commit = review['commit_id']
         state = review['state']
   
-        if state == 'APPROVED' and commit != self.target_commit_:
+        if state == 'APPROVED' and commit != self._target_commit:
           # An approval clears out any past rejections from a user.
           try:
             del reviews[user]
@@ -87,7 +87,7 @@ class PullRequest:
       else:
         return reviews
 
-  def base_pr_url_(self):
+  def _base_pr_url(self):
     # This is configured in nginx like:
     #
     # proxy_cache_path
@@ -124,15 +124,15 @@ class PullRequest:
     # them or make the dashboard only gather reviews for PRs in the "reviewme"
     # state.
     return 'https://www.jefftk.com/nomic-github/repos/%s/pulls/%s' % (
-      self.repo_, self.pr_number_)
+      self._repo, self._pr_number)
 
   def diff(self):
     response = util.request('https://patch-diff.githubusercontent.com/raw/%s/pull/%s.diff' % (
-        self.repo_, self.pr_number_))
+        self._repo, self._pr_number))
     return unidiff.PatchSet(response.content.decode('utf-8'))
 
   def author(self):
-    return self.pr_json_['user']['login']
+    return self._pr_json['user']['login']
 
 
 
