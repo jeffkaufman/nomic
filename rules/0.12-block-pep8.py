6d51e0ed0b89b9eaece6a91804750c73b2a91100
Todd Nelling <tnelling@gmail.com>
2019-02-12 13:37:05 -0500

    Remove quiet mode so we can see the errors

--- rules/0.12-block-pep8.py
+++ rules/0.12-block-pep8.py
@@ -1,7 +1,7 @@
 import pep8
 
 def should_block(pr):
-  pep8style = pep8.StyleGuide(quiet=True)
+  pep8style = pep8.StyleGuide()
   result = pep8style.check_files('.')
   if result.total_errors > 0:
     raise Exception('pep8 failed')
